d31f0ebbeb858c38e1a8e906b7bcb32a467e2536
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-11-30 15:37:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,38 +21,5 @@ def get_db_connection():
         print(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
-# Exemplo de rota para obter todos os pacientes
-@app.route('/pacientes', methods=['GET'])
-def get_pacientes():
-    conn = get_db_connection()
-    if conn is None:
-        return jsonify({"message": "Erro na conexão com o banco de dados!"}), 500
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM pacientes")
-    pacientes = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    
-    # Convertendo os resultados para JSON
-    return jsonify(pacientes)
-
-# Rota para inserir um paciente
-@app.route('/inserir_paciente', methods=['POST'])
-def insert_paciente():
-    data = request.get_json()
-    conn = get_db_connection()
-    if conn is None:
-        return jsonify({"message": "Erro na conexão com o banco de dados!"}), 500
-    
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO pacientes (ID, Nome, Cpf, Restricoes, quartosID) VALUES (?, ?, ?, ?, ?)",
-                   (data['ID'], data['Nome'], data['Cpf'], data['Restricoes'], data['quartosID']))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    
-    return jsonify({"message": "Paciente inserido com sucesso!"}), 201
-
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
